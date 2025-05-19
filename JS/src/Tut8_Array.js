c980f55bcd8a88a64bea4cca7b30e8b52ac241b2
The-Enigma-Code <niteshchaudhary0812@gmail.com>
2025-05-19 18:40:32 +0530

    this commit will be get stored in demo branch

--- JS/src/Tut8_Array.js
+++ JS/src/Tut8_Array.js
@@ -4,18 +4,37 @@
 // }
 // for(let index in arr){//iterating over index
 //     console.log(index,arr[index])
+// // }
+
+//===================1st================
+// const arr = []
+// let i = 0;
+// let sum = 0;
+// while(true){
+//     let element = prompt("enter the value");
+//     if(!element){
+//         for (const element of arr) {
+//             sum = sum + element;
+//         }
+//         console.log(arr)
+//         console.log(`sum is: ${sum}`)
+//         break;
+//     }
+//     if(!isNaN(Number(element))){
+//         arr[i] =Number(element);
+//         i++; 
+//     }
 // }
-const arr = []
-let i = 0;
-while(true){
-    let element = prompt("enter the value");
-    if(!element){
-        console.log(arr)
-        break;
-    }
-    arr[i] = element;
-        i++; 
+//=======================2nd=============
+const number = Number(prompt("enter your Number"))
+const lastDigit = number%10;
+let temp = number;
+while(parseInt(temp/10)>0){
+    temp = parseInt(temp/10)
 }
+const firstDigit = temp;
+console.log('first digit: ',firstDigit)
+console.log('last digit: ',lastDigit)
 //firs last digit - and sum
 //10 input from array and sum of them 
 
